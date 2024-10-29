241d82fef47b54c1054139f36fb16ed880724726
designdoughdev <dev@designdough.co.uk>
2024-10-29 10:29:52 +0000

    change theme name

--- functions.php
+++ functions.php
@@ -2,36 +2,36 @@
 
 // Register styles
 
-function adams_wp_starter_register_styles()
+function dd_starter_register_styles()
 {
 
     $version = wp_get_theme()->get('Version');
 
-    wp_enqueue_style('adams_wp_starter', get_template_directory_uri() . '/dist/scss/main.css', array(), $version);
+    wp_enqueue_style('dd_starter', get_template_directory_uri() . '/dist/scss/main.css', array(), $version);
 }
 
-add_action('wp_enqueue_scripts', 'adams_wp_starter_register_styles');
+add_action('wp_enqueue_scripts', 'dd_starter_register_styles');
 
 
 // Register scripts
 
-function adams_wp_starter_register_scripts()
+function dd_starter_register_scripts()
 {
 
     $version = wp_get_theme()->get('Version');
 
-    wp_enqueue_script('adams_wp_starter-main', get_template_directory_uri() . '/dist/js/main.js', array(), $version, true);
+    wp_enqueue_script('dd_starter-main', get_template_directory_uri() . '/dist/js/main.js', array(), $version, true);
 }
 
-add_action('wp_enqueue_scripts', 'adams_wp_starter_register_scripts');
+add_action('wp_enqueue_scripts', 'dd_starter_register_scripts');
 
 // Theme Support
 
-function adams_wp_starter_theme_support()
+function dd_starter_theme_support()
 {
 
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
 }
 
-add_action('after_setup_theme', 'adams_wp_starter_theme_support');
+add_action('after_setup_theme', 'dd_starter_theme_support');
---
+++ readme.md
@@ -0,0 +1,14 @@
+# DD Starter Template 
+
+1. Clone repository
+
+
+```git clone https://github.com/designdoughdev/dd-starter-parcel.git```
+
+2. Enter theme directory then initilise
+
+```npm init -y```
+
+3. Then install dependencies with
+
+```npm i```
